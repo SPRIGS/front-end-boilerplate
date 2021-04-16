5d9a626bfa76b14d9013c0f3d6925edfd08db892
Afron <afron@sprigs.eu>
2021-04-16 14:48:13 +0200

    Update libraries, code cleanup

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,31 +1,24 @@
 const mix = require('laravel-mix');
+const path = require('path');
 
-mix.setPublicPath(path.resolve('./'));
-
-var HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
-mix.webpackConfig({
-    plugins: [new HardSourceWebpackPlugin()],
-});
+// mix.webpackConfig({
+// Add custom config here if neccessary
+// });
 
 mix.copy('resources/assets/js/vendor', 'public/assets/web/dist/js/vendor');
 
-//  mix.browserSync('domain.test');
+mix.browserSync(`${path.resolve(__dirname).split(path.sep).pop()}.test`);
 
 mix.options({
     processCssUrls: false,
 });
 
 mix.sass('resources/assets/sass/app.scss', 'public/assets/web/dist/css');
-if (mix.inProduction())
-    mix.sourceMaps(false, 'source-map')
-        // mix.styles(
-        //     ['public/assets/web/dist/css/app.css'],
-        //     'public/assets/web/dist/css/app.css',
-        //     './'
-        // );
-
-        .mix.version('public/assets/web/dist/css/app.css');
-
 mix.js('resources/assets/js/app.js', 'public/assets/web/dist/js');
 
-mix.version('public/assets/web/dist/js/app.js');
+if (!mix.inProduction()) {
+    mix.sourceMaps(false, 'source-map')
+} else {
+    mix.version('public/assets/web/dist/css/app.css');
+    mix.version('public/assets/web/dist/js/app.js');
+}
